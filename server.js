d19597fbcc540f02072f99dd5befdf0800c32f34
jayunani <j.ayunani@gmail.com>
2017-03-28 12:10:05 -0700

    reconfiguring port

--- server.js
+++ server.js
@@ -33,4 +33,6 @@ app.use(function(req, res) {
     }
   });
 });
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 3000, function() {
+  console.log('Express server listening on port 3000');
+});
